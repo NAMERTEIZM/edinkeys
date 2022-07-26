3b4db1daf910948ea030de991ff7962cb4840139
NAMERTEIZM <109963581+NAMERTEIZM@users.noreply.github.com>
2022-07-26 15:38:23 +0300

    Add files via upload

---
+++ login-page.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="edinkeys,login,edin,keys">
+	<link rel="stylesheet" href="style.css">
+    <title>Edinkeys</title>
+</head>
+<body>
+    <div class="container" id="login-container">
+<div id="login-screen" class="row">
+    <div id="photo" class="col-xl-4">
+        <img id="wp" src="img/wp.jpg">
+        </div>
+        <div id="girisekrani" class="col-xl-8">
+           
+    
+        <div class="col-xl-8" id="login-content ">
+     
+      <div id="forms" class="mb-3">
+      <div>
+        <img id="edinpng" src="img/edinkeys.png">
+     </div>
+     <div id="yazi">
+     Kurum eğitimlerinizi, daha kolay <p></p>
+     ve daha güçlü yönetin!
+     </div>
+     <p> </p>
+     
+     
+     <form action="" method="POST" class="signin-form">
+      <div class="formlar">
+      <label for="email">E-mail</label><p></p>
+      <input type="email" id="email" name="email">
+				<div id="pass" class="mb-3">
+                <label  for="pwd">Password:</label><p></p>
+             <input type="password" id="pwd" name="pwd" minlength="8"><br><br>
+				</div>
+      </div>
+      </form> 
+      <div id="alt" class="row"><p></p>
+      <div id="deneme" class="w-50 text-md-right " >
+										<a id="su" href="#">Sifremi Unuttum!</a>
+									</div>
+                                    <button id="gy" type="submit" name ="submit" value ="Giriş" class=" btn btn-primary" style="background-color:#2769b3; color:#FFF ">Giriş Yap</button>
+
+         
+    </div>		
+    </div>
+    
+      </div>
+      </div>
+
+      
+
+
+        </div>
+   
+   
+    </div>
+</div>
+
+</body>
+</html>
